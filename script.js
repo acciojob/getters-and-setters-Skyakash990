f603d83280baeb9eb012b00673d175fb0c975572
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-11 11:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,34 @@
 class Person {
-  private _name: string;
-  private _age: number;
-
-  constructor(name: string, age: number) {
-    this._name = name;
-    this._age = age;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get name(){
+    return this.name;
+  }
+  get age(){
+    return this.age;
   }
 
-  // Getter for name
-  get name(): string {
-    return this._name;
-  }
-
-  // Getter for age
-  get age(): number {
-    return this._age;
-  }
-
-  // Setter for age
-  set age(age: number) {
-    this._age = age;
+  set age(age) {
+    this.age = age;
   }
 }
 
 class Student extends Person {
-  study(): void {
+  study(){
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
-  teach(): void {
+  teach(){
     console.log(`${this.name} is teaching`);
   }
 }
 
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
 
-person.age = 30; // Using the setter to change the age
-console.log(person.age); // Output: 30
 
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
